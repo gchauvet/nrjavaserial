356d7c3f874d499c8057841ab43a91ff733f18d9
Guillaume Chauvet <g.chauvet@bimedia.com.fr>
2015-09-09 10:08:44 +0200

    Source code formatting

--- nrjavaserial/src/main/java/gnu/io/NativeResourceException.java
+++ nrjavaserial/src/main/java/gnu/io/NativeResourceException.java
@@ -1,17 +1,15 @@
 package gnu.io;
 
 public class NativeResourceException extends RuntimeException {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private String message;
-	
-	public NativeResourceException(String msg) {
-		message = msg;
-	}
-	
-	public String toString() {
-		return message;
-	}
+
+    private static final long serialVersionUID = 1L;
+    private String message;
+
+    public NativeResourceException(String msg) {
+        message = msg;
+    }
+
+    public String toString() {
+        return message;
+    }
 }
--- nrjavaserial/src/main/java/gnu/io/SerialManager.java
+++ nrjavaserial/src/main/java/gnu/io/SerialManager.java
@@ -1,20 +1,21 @@
 package gnu.io;
 
 public class SerialManager {
-	
-	private static SerialManager instance;
-	private static boolean loaded = false;
-	private SerialManager() throws NativeResourceException {
-		if(!loaded) {
-			loaded = true;
-			new NativeResource().load("libNRJavaSerial");	
-		}
-	}
-	
-	public static SerialManager getInstance() throws NativeResourceException {
-		if(instance == null) {
-			instance = new SerialManager();
-		}		
-		return instance;
-	}
+
+    private static SerialManager instance;
+    private static boolean loaded = false;
+
+    private SerialManager() throws NativeResourceException {
+        if (!loaded) {
+            loaded = true;
+            new NativeResource().load("libNRJavaSerial");
+        }
+    }
+
+    public static SerialManager getInstance() throws NativeResourceException {
+        if (instance == null) {
+            instance = new SerialManager();
+        }
+        return instance;
+    }
 }
